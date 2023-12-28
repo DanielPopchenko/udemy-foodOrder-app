2bfb8b241e81f57485c76960c2e448d8e5126249
Daniel Popchenko <104201822+DanielPopchenko@users.noreply.github.com>
2023-12-28 13:29:20 +0200

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
-import React from "react";
-
-const App = () => {
-	return <div>App</div>;
-};
+function App() {
+  return (
+    <>
+      <h1>You got this 💪</h1>
+      <p>Stuck? Not sure how to proceed?</p>
+      <p>Don't worry - we've all been there. Let's build it together!</p>
+    </>
+  );
+}
 
 export default App;
